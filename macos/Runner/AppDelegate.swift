12173a51be047c7f79ba3c54faa8748471c70216
Sergey C <sergey.c@protoq.dev>
2026-05-14 12:18:46 +0200

    feat: enhance URL handling in application delegate

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -10,4 +10,10 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
+
+  override func application(_ application: NSApplication, open urls: [URL]) {
+    NSApp.activate(ignoringOtherApps: true)
+    application.mainWindow?.makeKeyAndOrderFront(self)
+    super.application(application, open: urls)
+  }
 }
